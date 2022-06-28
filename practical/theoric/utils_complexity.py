e9cb05a80add895ba7acd74ddddb47657712ce33
antoine.boutin <antoine.boutin@epita.fr>
2022-06-28 16:21:16 +0200

    d

--- practical/theoric/utils_complexity.py
+++ practical/theoric/utils_complexity.py
@@ -1,9 +1,9 @@
 from imports import *
 
 edges_directed, edges_undirected = random_connected_graph(10, 15)
-graph_u = Graph(edges_undirected)
-graph_d = Graph(edges_directed)
-g_u = directed_graph_to_nxgraph(graph_u)
+graph_u = Graph(10, edges_undirected, False)
+graph_d = Graph(10, edges_directed, True)
+g_u = undirected_graph_to_nxgraph(graph_u)
 g_d = directed_graph_to_nxgraph(graph_d)
 show_graph(g_u)
 show_graph(g_d)
